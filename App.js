7179a6287d55c01cf5b6bfa32056d62aefed32c5
James <jimibue@gmail.com>
2022-01-24 09:09:24 -0800

    init commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>App component here</Text>
+        <Text>App component here</Text>
+        <Text>App component here</Text>
+        <Text>App component here</Text>
+        <Text>App component here!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
